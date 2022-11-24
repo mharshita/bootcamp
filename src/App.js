28e7da4e6283db651d1a2cbce12ea4d3114d59e4
Harshita Mangla <manglaharshita@gmail.com>
2022-11-24 11:08:48 +0530

    session-2 assignment

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading2 = React.createElement("h1", {}, "Hello World! - from React");
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(heading2);
